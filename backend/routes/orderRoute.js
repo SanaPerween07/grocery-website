38ebcbd4c4ae3ef4116376f06f428ff0b19ce3a1
SanaPerween07 <sanahmad15046@gmail.com>
2025-04-14 15:54:21 +0530

    body parser fixed

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,15 +1,15 @@
 import express from 'express'
 import authUser from '../middlewares/authUser.js'
-import { getAllOrders, getUserOrders, placeOrderCOD, placeOrderStripe } from '../controllers/orderController.js'
+import { getAllOrders, getUserOrders, placeOrderCOD, placeOrderStripe, stripeWebHooks } from '../controllers/orderController.js'
 import authSeller from '../middlewares/authSeller.js'
+import bodyParser from 'body-parser' 
 
 const orderRouter = express.Router()
 
-orderRouter.post('/cod' , authUser, placeOrderCOD)
-orderRouter.post('/user' , authUser, getUserOrders)
-orderRouter.post('/seller' , authSeller, getAllOrders)
-orderRouter.post('/stripe' , authUser, placeOrderStripe)
+orderRouter.post('/cod', authUser, placeOrderCOD)
+orderRouter.post('/user', authUser, getUserOrders)
+orderRouter.post('/seller', authSeller, getAllOrders)
+orderRouter.post('/stripe', authUser, placeOrderStripe)
 orderRouter.post('/webhook', bodyParser.raw({ type: 'application/json' }), stripeWebHooks)
 
-
-export default orderRouter
+export default orderRouter 
