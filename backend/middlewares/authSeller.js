91e5b2578f0db2b57fbeb03594e0851a11b07b77
SanaPerween07 <sanahmad15046@gmail.com>
2025-04-14 16:15:37 +0530

    changes fixed

--- backend/middlewares/authSeller.js
+++ backend/middlewares/authSeller.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 const authSeller = async (req, res,next) => {
-    const {sellerToken} = req.cookies.sellerToken
+    const sellerToken = req.cookies.sellerToken
 
     if(!sellerToken){
         return res.json({success: false, message: "Not Authorized"})
