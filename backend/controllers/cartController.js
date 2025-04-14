25118ddcec148c9c199e5dde885eb5b2ed22ce6a
SanaPerween07 <sanahmad15046@gmail.com>
2025-04-14 16:54:22 +0530

    change

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -6,8 +6,7 @@ export const updateCart = async (req, res)=> {
     
     try{
         const {userId, cartItems} = req.body
-        await User.findByIdAndUpdate(userId, { cartItems }, { new: true });
-
+        await User.findByIdAndUpdate(userId, {cartItems})
         res.json({success:true, message: "Cart Updated"})
     }
     catch (error) {
